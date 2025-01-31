ffab902140d4885bb2c4ff64b7a6e0d3abe66ae6
Dave Calnan <dave.calnan@telea.co>
2025-01-31 21:19:02 +0000

    Yikes - auth much?

--- apps/backend/bootstrap/app.php
+++ apps/backend/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(append: ['auth:sanctum']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
