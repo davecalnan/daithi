1b642eb2ff73601d29bd6c208dc0cd14c6fc9cd0
Dave Calnan <dave.calnan@telea.co>
2025-01-31 17:36:45 +0000

    Force HTTPS

--- apps/backend/bootstrap/app.php
+++ apps/backend/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->booted(function () {
+        URL::forceHttps(app()->isProduction());
+
         $api = Route::middleware('api');
 
         if (app()->isProduction()) {
